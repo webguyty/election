05b4268d2f940cf791431771bf62c44840dfae39
Tyler <tyler@webguyty.com>
2021-05-07 10:28:17 -0700

    deploy of election contract working

---
+++ contracts/Election.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+  // Store candidate
+  // Read Candidate
+
+  string public candidate;
+
+  // Constructor 
+  // function Election () public {
+  //   candidate = "Candidate 1";
+  // }
+  constructor () public {
+    candidate = "Candidate 1";
+  }
+}
